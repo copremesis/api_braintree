ed807e1b1fdd84efc9f987af8350e2224af86f10
Rob <robert.a.ortiz@gmail.com>
2013-12-02 16:01:38 -0600

    adding braintree script with ideas for exercises

---
+++ braintree.rb
@@ -0,0 +1,46 @@
+require "rubygems"
+require "braintree"
+require "awesome_print"
+require "json"
+require "faker"
+
+
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = "8r9rgh86r35v6dpd"
+Braintree::Configuration.public_key = "kvt5jjxkm99yt55b"
+Braintree::Configuration.private_key = "b0a52c8b0f6bf1ee361d78a6dbe061f5"
+
+
+@sale = ->() {
+  result = Braintree::Transaction.sale(
+    :amount => rand(5..10).to_s + ".00" ,
+    :order_id => "order id",
+    :credit_card => {
+      :number => "5105105105105100",
+      :expiration_date => "05/2012",
+      :cvv => "111"
+    },
+    :customer => {
+      #:first_name => Faker::Name.first_name,
+      #:last_name => Faker::Name.last_name,
+      :first_name => "",
+      :last_name => "",
+      :company => "MakerSquare",
+      :website => "http://www.themakersquare.com",
+      :email => ""
+    }
+  )
+}
+
+@sale[]
+
+#api exercises:
+
+
+#Xml.hash_from_xml('<xml><person><name>Rob</name></person></xml>')
+#ruby -> xml
+#rexeml, nokogiri, xml_simple ... native ruby
+#result = Braintree::Transaction.void("6pdjq6")
+#json
+#JSON.parse
+#object.to_json
---
+++ workshop.md
@@ -0,0 +1,28 @@
+
+workshop.rb
+
+API workshop
+
+1. easy
+
+  * download gem
+  * review documentation
+  * run script
+
+2. intermidiate
+
+  * serialization
+    + xml <-> ruby
+    + JSON <->ruby
+    + legacy key=value& ...
+
+3. exercises
+
+  * fork this repo
+  * get your name & email on the dash board
+
+4. challenges
+   
+  * can you remove you name from the dash board?
+    (void transaction?) 
+  * parsing xml, json and connecting to 3rd parties
