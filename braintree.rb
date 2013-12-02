59bf16b799e09935d45f7aba2adc9a721c3f3c4e
Rob <robert.a.ortiz@gmail.com>
2013-12-02 17:57:12 -0600

    finalizing todays exercise ... students seem really cool and young

--- braintree.rb
+++ braintree.rb
@@ -10,35 +10,35 @@ Braintree::Configuration.merchant_id = "8r9rgh86r35v6dpd"
 Braintree::Configuration.public_key = "kvt5jjxkm99yt55b"
 Braintree::Configuration.private_key = "b0a52c8b0f6bf1ee361d78a6dbe061f5"
 
+#exception handling
+def output_results(result) 
+  if result.success?
+    ap result
+    puts "success!: #{result.transaction.id}"
+  elsif result.transaction
+    puts "Error processing transaction:"
+    puts "  code: #{result.transaction.processor_response_code}"
+    puts "  text: #{result.transaction.processor_response_text}"
+  else
+    p result.errors
+  end
+end
 
-@sale = ->() {
-  result = Braintree::Transaction.sale(
-    :amount => rand(5..10).to_s + ".00" ,
-    :order_id => "order id",
-    :credit_card => {
-      :number => "5105105105105100",
-      :expiration_date => "05/2012",
-      :cvv => "1111"
-    },
-    :customer => {
-      :first_name => "",
-      :last_name => "",
-      :company => "MakerSquare",
-      :website => "http://www.themakersquare.com",
-      :email => ""
-    }
-  )
+result = Braintree::Transaction.sale(
+  :amount => rand(5..10).to_s + ".00" ,
+  :order_id => "order id",
+  :credit_card => {
+    :number => "5105105105105100",
+    :expiration_date => "05/2012",
+    :cvv => "1111"
+  },
+  :customer => {
+    :first_name => "",
+    :last_name => "",
+    :company => "MakerSquare",
+    :website => "http://www.themakersquare.com",
+    :email => ""
+  }
+)
 
-  ap result
-}
-
-@sale[]
-
-#api exercises:
-#Xml.hash_from_xml('<xml><person><name>Rob</name></person></xml>')
-#ruby -> xml
-#rexeml, nokogiri, xml_simple ... native ruby
-#result = Braintree::Transaction.void("6pdjq6")
-#json
-#JSON.parse
-#object.to_json
+output_results(result)
